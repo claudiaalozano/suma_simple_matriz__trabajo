d2beb8788afcd44b3c697ccb898e7192b7c7e7d4
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-14 20:36:09 +0100

    creamos bucle para añadir los elementos a la matriz

--- suma_simple_matriz.py
+++ suma_simple_matriz.py
@@ -2,3 +2,8 @@ numero_filas = print(input("Seleccione el número de filas de la matriz: "))
 numero_columnas = print(input("Seleccione el número de columnas de la matriz: "))
 
 matriz = []
+
+for i in range(numero_filas):
+    matriz.append([])
+    for j in range(numero_columnas):
+        matriz[i].append(None)
