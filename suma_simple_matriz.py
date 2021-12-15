a24fe24a3c6a4339bc5ccad1e7ee9c8c9980ef4e
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-15 17:28:34 +0100

    mejoras en el código de antes

--- suma_simple_matriz.py
+++ suma_simple_matriz.py
@@ -1,9 +1,11 @@
-numero_filas = print(input("Seleccione el número de filas de la matriz: "))
-numero_columnas = print(input("Seleccione el número de columnas de la matriz: "))
+from random import randint
+
+numero_filas = int(input("Seleccione el número de filas de la matriz: "))
+numero_columnas = int(input("Seleccione el número de columnas de la matriz: "))
 
 matriz = []
-
 for i in range(numero_filas):
     matriz.append([])
     for j in range(numero_columnas):
-        matriz[i].append(None)
+        matriz[i].append(int(randint(0,100)))
+        
