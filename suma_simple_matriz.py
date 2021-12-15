9b4d8a7b03c98b65d4810c410e9bf21881aa7f15
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-15 17:46:26 +0100

    código para sumar los elementos de la matriz

--- suma_simple_matriz.py
+++ suma_simple_matriz.py
@@ -8,4 +8,12 @@ for i in range(numero_filas):
     matriz.append([])
     for j in range(numero_columnas):
         matriz[i].append(int(randint(0,100)))
-        
+
+suma = 0 
+
+for i in range(numero_filas):
+    for j in range(numero_columnas):
+        suma = suma + matriz[i][j]
+        suma = int(suma)
+print(matriz)
+print(suma)
